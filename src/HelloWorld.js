95f6fe07e1d6c8dc12c4baa6dfb33d5573998f9b
Wizzywit <wisdompraise968@gmail.com>
2020-04-02 20:05:07 +0100

    first commit

---
+++ src/HelloWorld.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function HelloWorld() {
+    return (
+        <div>
+            <h1>HelloWorld</h1>
+            <ul>
+                <li>One</li>
+                <li>Two</li>
+                <li>Three</li>
+                <li>Four</li>
+            </ul>
+        </div>
+    )
+}
+
+export default HelloWorld;
